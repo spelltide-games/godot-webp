9c26d328a677368eb5af728897c6f8a1ea31355a
blueloveTH <blueloveth@foxmail.com>
2026-08-02 13:26:34 +0800

    bak

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -21,7 +21,7 @@ static void uninitialize(ModuleInitializationLevel p_level) {
 
 extern "C" {
 // Initialization.
-GDExtensionBool GDE_EXPORT godot_pocketpy_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+GDExtensionBool GDE_EXPORT godot_webp_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
 	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
 	init_obj.register_initializer(initialize);
